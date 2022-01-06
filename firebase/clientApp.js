8db7892be54b5319c9e345eda645025cd0cf77a8
Mathieu Desjarlais <github@desmat.ca>
2022-01-05 20:18:15 -0500

    debug next.js build/deploy

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase/app'
 import 'firebase/auth' // If you need it
 import 'firebase/firestore' // If you need it
-import 'firebase/storage' // If you need it
-import 'firebase/analytics' // If you need it
-import 'firebase/performance' // If you need it
+// import 'firebase/storage' // If you need it
+// import 'firebase/analytics' // If you need it
+// import 'firebase/performance' // If you need it
 
 const clientCredentials = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,7 +12,7 @@ const clientCredentials = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 if (!firebase.apps.length) {
@@ -20,10 +20,10 @@ if (!firebase.apps.length) {
   // Check that `window` is in scope for the analytics module!
   if (typeof window !== 'undefined') {
     // Enable analytics. https://firebase.google.com/docs/analytics/get-started
-    if ('measurementId' in clientCredentials && process.env.NODE_ENV !== 'development') {
-      firebase.analytics()
-      firebase.performance()
-    }
+    // if ('measurementId' in clientCredentials && process.env.NODE_ENV !== 'development') {
+    //   firebase.analytics()
+    //   firebase.performance()
+    // }
   }
 }
 
