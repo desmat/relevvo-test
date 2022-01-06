078df923619a18f6114f27ee16bfcff9964612f3
Mathieu Desjarlais <github@desmat.ca>
2022-01-06 00:09:04 -0500

    Entity: store.addLike

--- fetchData/stores.js
+++ fetchData/stores.js
@@ -13,6 +13,34 @@ export const getStore = async (id) => {
   return doc.data()
 }
 
+export const addLike = async (id) => {
+  console.log('addLike', { id } )
+
+  return new Promise((resolve, reject) => {
+    const db = firebase.firestore()
+    const collection = db.collection('stores')
+
+    if (id) {
+      collection.doc(id).get()
+        .then((doc) => {
+          if (doc.exists) {
+            const updatedData = {             
+              likes: (doc.data().likes || 0) + 1,
+              updated: new Date(), //serverTimestamp() // TODO UPGRADE TO NEWER FIRESTORE VERSION
+            }
+            doc.ref.update(updatedData).then(() => {
+              resolve({ ...doc.data(), ...updatedData })
+            })
+          } else {
+            reject(`Error saving store: not found: ${id}`)
+          }
+        })
+    } else {
+      reject(`Error saving store: not found: ${id}`)
+    }
+  })
+}
+
 export const getStores = (onSuccess) => {
   console.log('getStores')
   const db = firebase.firestore()
--- pages/stores/[id].js
+++ pages/stores/[id].js
@@ -2,12 +2,22 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
-import { getStore } from '../../fetchData/stores'
+import { getStore, addLike } from '../../fetchData/stores'
 
 export default function Page() {
   const router = useRouter()
   const { id } = router.query
   const [store, setStore] = useState({})
+
+  const like = () => {
+    if (store) {
+      addLike(id).then((updatedStore) => {
+        setStore({ ...updatedStore, loaded: true })
+      }).catch(console.error)
+      // anticipate
+      setStore({ ...store, likes: (store.likes || 0) + 1 })
+    }
+  }
 
   useEffect(() => {
     if (id) {
@@ -30,14 +40,19 @@ export default function Page() {
 
       <main>
         <h1 className="title">Store</h1>
-        <ul>
-          { store.loaded && 
+        { store.loaded && 
+          <ul>
             <li>
               <b>name:</b> {store.name}
+              </li>
+            <li>
+              <b>likes:</b> {store.likes || 0}
             </li>
-          }   
-        </ul>      
+          </ul>      
+        }   
       </main>
+
+      <p><a style={{ cursor: 'pointer' }} onClick={like}>LIKE</a></p>
 
       <Link href="/stores" passHref>
         <a>Back</a>
